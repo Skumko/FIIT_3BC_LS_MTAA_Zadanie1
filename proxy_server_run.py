c23e3630c89719147042d0167665bc5864f4f3a1
Pavol Belej <skumko21@gmail.com>
2022-02-20 15:23:26 +0100

    initial commit with first version of assignment

---
+++ proxy_server_run.py
@@ -0,0 +1,33 @@
+import proxy
+import socketserver
+import socket
+import sys
+import time
+import logging
+
+
+def logging_setup():
+    logging.basicConfig(format='%(asctime)s:%(levelname)s:%(message)s', filename='proxy_server.log', level=logging.INFO,
+                        datefmt='%H:%M:%S')
+    logging.info(time.strftime("%a, %d %b %Y %H:%M:%S ", time.localtime()))
+
+
+def main_test():
+    logging_setup()
+
+    hostname = socket.gethostname()
+    ipaddress = socket.gethostbyname(hostname)
+    if ipaddress == "126.0.0.1":
+        ipaddress = sys.argv[0]
+
+    logging.info(hostname)
+    logging.info(ipaddress)
+
+    proxy.recordroute = f"Record-Route: <sip:{ipaddress}:{proxy.PORT};lr>"
+    proxy.topvia = f"Via: SIP/2.0/UDP {ipaddress}:{proxy.PORT}"
+    server = socketserver.UDPServer((proxy.HOST, proxy.PORT), proxy.UDPHandler)
+    server.serve_forever()
+
+
+if __name__ == '__main__':
+    main_test()
